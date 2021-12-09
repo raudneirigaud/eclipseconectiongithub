c15537525b0b421ff8631c439ca44302cdc7985b
SUPORTE12 <SUPORTE12@DESKTOP-ND5F5A7>
2021-12-09 00:40:16 -0300

    Primeiro commit

--- src/spread/Java.java
+++ src/spread/Java.java
@@ -5,6 +5,7 @@ public class Java {
 	public static void main(String[] args) {
 		
 		System.out.println("Teste integração Eclipse com Github");
+		System.out.println("Teste integração Eclipse com Github - teste");
 	}
 
 }
